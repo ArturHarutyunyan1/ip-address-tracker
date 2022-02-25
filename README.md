3fec97e562071373da8436a4152f8821ac503a26
Artur Harutyunyan <76475750+ArturHarutyunyan1@users.noreply.github.com>
2022-02-26 00:35:36 +0400

    Add files via upload

---
+++ README.md
@@ -0,0 +1,45 @@
+# Frontend Mentor - IP address tracker solution
+
+This is a solution to the [IP address tracker challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/ip-address-tracker-I8-0yYAH0). Frontend Mentor challenges help you improve your coding skills by building realistic projects. 
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+- [Author](#author)
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- View the optimal layout for each page depending on their device's screen size
+- See hover states for all interactive elements on the page
+- See their own IP address on the map on the initial page load
+- Search for any IP addresses or domains and see the key information and location
+
+### Screenshot
+
+![](img/desktop-preview.jpg)
+
+### Links
+
+- Solution [URL](https://www.frontendmentor.io/challenges/ip-address-tracker-I8-0yYAH0)
+- Live Site [URL](https://arturharutyunyan1.github.io/ip-address-tracker/)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- JavaScript
+
+## Author
+
+- Frontend Mentor - [@ArturHarutyunyan1](https://www.frontendmentor.io/profile/ArturHarutyunyan1)
---
+++ script.js
@@ -0,0 +1,41 @@
+const inputForm = document.querySelector('.form')
+const inputValue = document.querySelector('#ipAddress')
+const currentIP = document.querySelector('.currentIP')
+const currentLocation = document.querySelector('.currentLocation')
+const currentTimeZone = document.querySelector('.currentTimezone')
+const currentISP = document.querySelector('.currentIsp')
+
+inputForm.addEventListener('submit', (e)=>{
+    e.preventDefault()
+    fetch(`https://geo.ipify.org/api/v2/country,city,vpn?apiKey=at_XEzVrz8JJfWUBeOcbiaZliva9S9sF&ipAddress=${inputValue.value}`)
+    .then((res) => res.json())
+    .then((data) => updateMap(data))
+})
+
+function updateMap(data){
+    let {lat} = data.location
+    let {lng} = data.location
+    map.setView([lat, lng], 13)
+    L.marker([lat, lng], {icon: customMarker}).addTo(map)
+    console.log(data);
+    currentIP.innerHTML = `${data.ip}`
+    currentLocation.innerHTML = `${data.location.city}`
+    currentTimeZone.innerHTML = `UTC ${data.location.timezone}`
+    currentISP.innerHTML = `${data.isp}`
+}
+
+let lat = 51.50853
+let lng = -0.12574
+let map = L.map('map').setView([lat, lng], 13);
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: 'pk.eyJ1IjoiYXJ0dXJoYXJ1dHl1bnlhbiIsImEiOiJja3p3YjVzdnc2dHdlMm5vMXlicmlsajYyIn0.tr-NCvCtkVSLGXqJP3lJmQ'
+}).addTo(map);
+let customMarker = L.icon({
+    iconUrl: 'img/icon-location.svg'
+})
+let marker = L.marker([lat, lng], {icon: customMarker}).addTo(map);
